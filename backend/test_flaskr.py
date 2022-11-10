a921d2121bb0f13d41815eceec96c0fa865977f2
Akele Belay <akelebch12@gmail.com>
2022-11-10 07:04:35 +0300

    Update test_flaskr.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -4,26 +4,7 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-from models import setup_db, Question, Category
-
-import sys
-import traceback
-
-try:
-    assert True
-    assert 7 == 7
-    assert 1 == 2
-    # many more statements like this
-except AssertionError:
-    _, _, tb = sys.exc_info()
-    traceback.print_tb(tb) # Fixed format
-    tb_info = traceback.extract_tb(tb)
-    filename, line, func, text = tb_info[-1]
-
-    print('An error occurred on line {} in statement {}'.format(line, text))
-    exit(1)
-    
-
+from models import setup_db, Question, Category 
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
