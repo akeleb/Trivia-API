2e02c33e8ff177ffcb32eee30e2c3d6023b26d8e
Akele Belay <akelebch12@gmail.com>
2022-11-10 09:22:57 +0000

    test completed

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -4,16 +4,7 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-<<<<<<< HEAD
 from models import setup_db, Question, Category
-
-import sys
-import traceback
-    
-=======
-from models import setup_db, Question, Category 
-
->>>>>>> a921d2121bb0f13d41815eceec96c0fa865977f2
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
@@ -22,7 +13,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql+psycopg2://postgres:Aklexsqldb14@localhost:5432/udacityexc".format('localhost:5432',self.database_name)
+        self.database_path = "postgresql+psycopg2://postgres://{}:{}@{}/{}".format('localhost:5432',self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
